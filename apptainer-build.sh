661ef79c848aa8466c1051216f26700a6a4a41c7
Viktor Kovtun <viktor.kovtun@unibe.ch>
2025-03-27 11:39:20 +0100

    Fixing not working build of the image.

--- apptainer-build.sh
+++ apptainer-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# This is a script to build the Apprainer sif file
+# This is a script to build the Apptainer sif file
 
 set -x
+cd src
 apptainer build --fakeroot streamlit.sif src/streamlit.def
